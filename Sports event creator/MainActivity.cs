74b414a3fcf72b82f010c988bfead2699274cc79
jadelsky <jakovdelas@yahoo.com>
2020-01-30 23:25:25 +0100

    Adding activity_login.xml

--- Sports event creator/MainActivity.cs
+++ Sports event creator/MainActivity.cs
@@ -14,7 +14,7 @@ namespace Sports_event_creator
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.activity_login);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
